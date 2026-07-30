45a7fc839280fc75867474d46484ce9d897e3f80
Arenasxas <nicoivanarenas@gmail.com>
2026-07-30 17:07:04 -0300

    feat: Fase 6 tutor adaptativo - catalogo curado de contenido real (series/musica/libros/podcasts) por nivel e intereses

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "clase-ingles-v7";
+const CACHE = "clase-ingles-v8";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", e => {
