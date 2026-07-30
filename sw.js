8e55c6db6123ff84dbb6a428eeded2787d9f8686
Arenasxas <nicoivanarenas@gmail.com>
2026-07-30 13:28:36 -0300

    feat: Fase 3 tutor adaptativo - onboarding needs-analysis + diagnostico adaptativo + prescripcion (reemplaza test de estilos VAK)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "clase-ingles-v4";
+const CACHE = "clase-ingles-v5";
 const SHELL = ["./", "./index.html", "./manifest.webmanifest", "./icon.svg"];
 
 self.addEventListener("install", e => {
